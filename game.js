5ab5b20f69b0766fb909f4b447d8d173fe5932bd
anusca <anna.arsentieva@gmail.com>
2020-04-29 13:24:48 -0700

    implemented an event listener for the callback button to update the question, answer, value and catergory title divs with the information obtained from the callback in case of a successful invokation otherwise console the error

--- game.js
+++ game.js
@@ -1,1 +1,21 @@
-import getClue from "./callback-version.js";
+import getClueFromCallback from "./callback-version.js";
+window.addEventListener("DOMContentLoaded", (event) => {
+  let cbBtn = document.getElementById("use-callback");
+  let questionDiv = document.getElementById("question");
+  let answerDiv = document.getElementById("answer");
+  let valueDiv = document.getElementById("value");
+  let catTitleDiv = document.getElementById("category-title");
+
+  cbBtn.addEventListener("click", (event) => {
+    getClueFromCallback((error, clue) => {
+      if (error !== null) {
+        console.error(error);
+      } else {
+        questionDiv.innerHTML = clue.question;
+        answerDiv.innerHTML = clue.answer;
+        valueDiv.innerHTML = clue.value;
+        catTitleDiv.innerHTML = clue.category.title;
+      }
+    });
+  });
+});
