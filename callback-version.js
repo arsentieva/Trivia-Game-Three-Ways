42b210368e235d921de174a318bf774ba9fd1c92
anusca <anna.arsentieva@gmail.com>
2020-04-29 13:06:49 -0700

    in getClue method, we created httprequest instance and added event listener to retrieve the JSON when status code is 200, and send it

--- callback-version.js
+++ callback-version.js
@@ -1,1 +1,18 @@
-export function getClue(cb) {}
+export function getClue(cb) {
+  const httpRequest = new XMLHttpRequest();
+  httpRequest.addEventListener("readystatechange", (event) => {
+    if (httpRequest.readyState !== XMLHttpRequest.DONE) {
+      return;
+    }
+
+    if (httpRequest.status !== 200) {
+      cb();
+    } else {
+      const response = JSON.parse(httpRequest.responseText);
+      cb(null, response);
+    }
+  });
+
+  httpRequest.open("GET", "https://jservice.xyz/api/random-clue");
+  httpRequest.send();
+}
